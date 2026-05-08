de5f28da50590602bd769201f2f17f91b821d375
Linden Wells <linden.wells@deswik.com>
2026-05-08 16:25:30 +1000

    Split out opposite point logic into function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
+const digToChina = (point) => ({ lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360) })
+
 let point = { lat: -27.466206, lng: 153.024819, color: "red" }
-const opposite = { lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360), color: "blue" }
+const opposite = { ...digToChina(point), color: "blue" }
 
 
 const points = [point, opposite].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5}))
@@ -12,6 +14,6 @@ const globe = new Globe(document.getElementById('globeViz'))
     .pointColor('color')
     .onGlobeClick(({ lat, lng }) => {
         point = { ...point, lat, lng };
-        globe.pointsData([point, { lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360), color: "blue"}].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5})))
+        globe.pointsData([point, { ...digToChina(point), color: "blue"}].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5})))
     });
 
