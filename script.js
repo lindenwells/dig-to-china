6828769ca9d5dccdc8c06441e5a8e8d480bdc6d1
Linden Wells <linden.wells@deswik.com>
2026-05-08 15:32:10 +1000

    Add dynamic points via clicking

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
-const point = { lat: -27.466206, lng: 153.024819, color: "red" }
-const opposite = { lat: -point.lat, lng: ((point.lng -Math.sign(point.lng) * 180) % 360), color: "blue" }
+let point = { lat: -27.466206, lng: 153.024819, color: "red" }
+const opposite = { lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360), color: "blue" }
+
 
 const points = [point, opposite].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5}))
 
-new Globe(document.getElementById('globeViz'))
+const globe = new Globe(document.getElementById('globeViz'))
     .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-day.jpg')
     .pointsData(points)
     .pointAltitude('size')
-    .pointColor('color');
+    .pointColor('color')
+    .onGlobeClick(({ lat, lng }) => {
+        point = { ...point, lat, lng };
+        globe.pointsData([point, { lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360), color: "blue"}].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5})))
+    });
+
