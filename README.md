064b09a570f8d273354a9dd9c33dfcc75a1fdf11
Linden Wells <linden.wells@deswik.com>
2026-05-15 16:16:05 +1000

    Use vite and typescript

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Ever wondered where you would end up if you dug straight down from somewhere? No? Well now you can.
 
-To run locally: `npx http-server .`
+To run locally: `npm install` and `npm run dev`
 
 https://github.com/user-attachments/assets/a4ea8a35-7acc-421b-83e9-3d575ae81b11
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import Globe from "globe.gl";
 
-const digToChina = (point) => ({ lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360) })
+const digToChina = (point: { lat: number;  lng: number}) => ({ lat: -point.lat, lng: ((point.lng - Math.sign(point.lng) * 180) % 360) })
 
 let point = { lat: -27.466206, lng: 153.024819, color: "red" }
 const opposite = { ...digToChina(point), color: "blue" }
@@ -7,7 +8,7 @@ const opposite = { ...digToChina(point), color: "blue" }
 
 const points = [point, opposite].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5}))
 
-const globe = new Globe(document.getElementById('globeViz'))
+const globe = new Globe(document.getElementById('app')!)
     .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-day.jpg')
     .pointsData(points)
     .pointAltitude('size')
@@ -16,4 +17,3 @@ const globe = new Globe(document.getElementById('globeViz'))
         point = { ...point, lat, lng };
         globe.pointsData([point, { ...digToChina(point), color: "blue"}].map(({ lat, lng, color }) => ({lat, lng, color, size: 0.5})))
     });
-
